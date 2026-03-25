b902dbe70e9047845564873d50022e60452c1c1d
thanasis theodwropoulos <trikos529@gmail.com>
2026-03-25 20:57:21 +0200

    custom board
    custom board that types the id of a btn  just ad apropriate ids to your boards and you can type any language

---
+++ custom.py
@@ -0,0 +1,37 @@
+import asyncio
+import websockets
+import json
+import pyautogui
+
+id_toggle = False
+class_toggle = False
+
+
+async def main():
+    async with websockets.serve(handle_droidpad, "0.0.0.0", 8765):
+        print(f"WebSocket server running on ws://0.0.0.0:8765")
+        print("Press Ctrl+C to stop the server")
+        await asyncio.Future()
+
+async def handle_droidpad(websocket):
+    global id_toggle, class_toggle, classification, basic_tags, video_audio_tags, double_quotes, css_properties, typescript_methods
+    print("Device Linked!")
+    try:
+        async for message in websocket:
+            try:
+                data = json.loads(message)
+                msg_type = data.get("type")
+                state = data.get("state")
+                btn_id = data.get("id")
+                print(f"Received: {btn_id}")
+                pyautogui.write(btn_id)
+            except websockets.exceptions.ConnectionClosed:
+              print("Device disconnected")
+    except Exception as e:
+        print(f"Error in connection: {e}")
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nServer stopped by user")
